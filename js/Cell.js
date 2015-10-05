bda723ba716462cb0690b07724b63e074d234e68
Lergassi <eldred.king@gmail.com>
2015-10-05 10:14:02 +0000

    grid. rotate grid.

--- js/Cell.js
+++ js/Cell.js
@@ -2,20 +2,46 @@
  * Created on 05.10.2015.
  */
 
-var Cell = function(points, context)
+var Cell = function(points, context, params)
 {
     this.context = context;
     this.origin = points;
 
+    this.center = {
+        x:(this.origin[0].x + this.origin[2].x) / 2,
+        y:(this.origin[0].y + this.origin[2].y) / 2
+    };
+
+    //this.color = params.color;
+    //console.log(this.context);
     //this.scale(this.origin[2]);
     //this.moveToCenter();
-    this.rotate();
-    this.scale();
+    //this.rotate();
+    //this.scale();
     //this.move();
+
+    //this.move = function(offset)
+    //{
+    //    if(!offset)
+    //    {
+    //        var offset = {x: 70, y: 70};
+    //    }
+    //    this.origin.forEach(function(item, i, that){
+    //
+    //        (function() {
+    //            var point = {};
+    //            point.x = 1 * item.x + 0 * item.y + offset.x * 1;
+    //            point.y = 0 * item.x + 1 * item.y + offset.y * 1;
+    //            console.log(point);
+    //            that[i] = point;
+    //        })();
+    //    });
+    //}
 }
 
 Cell.prototype.draw = function()
 {
+    //console.log(this.context);
     this.context.beginPath();
     this.context.moveTo(this.origin[0].x, this.origin[0].y);
     for(var i = 1; i < this.origin.length; i++)
@@ -27,6 +53,12 @@ Cell.prototype.draw = function()
     this.context.closePath();
     this.context.fillStyle = "red";
     this.context.stroke();
+
+    this.context.beginPath();
+    console.log(this.center);
+    this.context.moveTo(this.center.x, this.center.y);
+    this.context.lineTo(this.center.x + 1, this.center.y + 1);
+    this.context.closePath();
 }
 
 Cell.prototype.scale = function()
@@ -59,15 +91,18 @@ Cell.prototype.rotate = function()
     });
 }
 
-Cell.prototype.move = function(point)
+Cell.prototype.move = function(offset)
 {
+    if(!offset)
+    {
+        var offset = {x: 100, y: 100};
+    }
     this.origin.forEach(function(item, i, that){
-        var a = 70;
-        var b = 70;
+
         (function() {
             var point = {};
-            point.x = 1 * item.x + 0 * item.y + a * 1;
-            point.y = 0 * item.x + 1 * item.y + b * 1;
+            point.x = 1 * item.x + 0 * item.y + offset.x * 1;
+            point.y = 0 * item.x + 1 * item.y + offset.y * 1;
             that[i] = point;
         })();
     });
--- js/Field.js
+++ js/Field.js
@@ -6,17 +6,49 @@ var Field = function(context)
 {
     this.context = context;
 
-    this.cell = new Cell([
+    this.patternCell = [
         {x:0, y: 0},
         {x:100, y: 0},
         {x:100, y: 100},
         {x:0, y: 100}
-    ], this.context);
+    ];
+
+    this.cell = new Cell(this.patternCell, this.context);
+    this.cells = [];
+    this.width = 20;
+    this.height = 12;
+    this.side = 50;
+
+    for(var i = 0; i < this.width; i++)
+    {
+        this.cells[i] = [];
+        for(var j = 0; j < this.height; j++)
+        {
+            var newCell = new Cell(this.patternCell.concat(), this.context);
+            this.cells[i][j] = newCell;
+            //this.cells[i][j].moveToCenter();
+            this.cells[i][j].move({x: 0 + this.side * i, y: 0 + this.side * j});
+            //this.cells[i][j].rotate();
+            //this.cells[i][j].scale();
+            //this.cells[i][j].move({x: 0, y: 350});
+        }
+    }
+
+    console.log(this.cells);
 };
 
 Field.prototype.draw = function()
 {
-    this.cell.draw();
-}
+    for(var i = 0; i < this.width; i++) {
 
+        for (var j = 0; j < this.height; j++) {
+            this.cells[i][j].draw();
+        }
+    }
+
+        //console.log(that)
+
+
+    //this.cell.draw();
+}
 
